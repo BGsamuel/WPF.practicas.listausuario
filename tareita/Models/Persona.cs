9c6cc306ce5f3dd764711945c530622632c2f7aa
ador samuel <samartinezm@outlook.com>
2020-05-04 22:20:59 -1100

    Agregar archivos de proyecto.

---
+++ tareita/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace WPF.Practices.Binding.Models
+{
+    public class Persona 
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Identificador { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Nombre, Apellido);
+        }
+    }
+}
---
+++ tareita/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.Practices.Binding.Models;
+namespace WPF.Practices.Binding.ViewModels
+{
+    public class UsuarioViewModel
+    {
+        public UsuarioViewModel()
+        {
+            Escuela = new Escuela();
+            Escuela.Nombre = "san nicolas";
+            Escuela.Personas = ObtenerAlumnos();
+        }
+        public Escuela Escuela { get; set; }
+
+        public List<Persona> ObtenerAlumnos()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Persona persona = new Persona();
+                persona.Nombre = "jorge" + i;
+                persona.Apellido = "maza" + i;
+                persona.Identificador = Guid.NewGuid().ToString();
+                personas.Add(persona);
+            }
+            return personas;
+        }
+
+    }
+}
+
